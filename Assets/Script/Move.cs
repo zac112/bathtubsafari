6b4e836180eb66d41d7ae728f034102f8160cef8
Erno Lokkila <eolokk@utu.fi>
2021-03-05 19:58:43 +0200

    initial commit

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown(){
+        gameObject.transform.position = new Vector3(transform.position.x+1, transform.position.y, transform.position.z);
+    }
+}
