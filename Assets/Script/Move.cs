e48866f820bef217dcf13f47c6a6060dbb05ff47
Tomas <saarinen.tomas@hotmail.fi>
2021-03-05 21:20:06 +0200

    Update Move.cs
    Move skripti editoitut

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
-{
+{  public float speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +12,24 @@ public class Move : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { Vector3 pos = transform.position;
+ 
+         if (Input.GetKey ("w")) {
+             pos.y += speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("s")) {
+             pos.y -= speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("d")) {
+             pos.x += speed * Time.deltaTime;
+         }
+         if (Input.GetKey ("a")) {
+             pos.x -= speed * Time.deltaTime;
+         }
+             
+ 
+         transform.position = pos;
+     
         
     }
 
