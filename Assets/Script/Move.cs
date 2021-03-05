1d5f8f094ac233db463041aca37b57fc61babf8d
Valtteri A <vjaarn@utu.fi>
2021-03-05 22:49:16 +0200

    Lisätty ankan kääntyminen liikkumissuuntaan

--- Assets/Script/Move.cs
+++ Assets/Script/Move.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Move : MonoBehaviour
-{  public float speed = 3.0f;
+{   public float speed = 3.0f;
+    public Sprite spriteUp;
+    public Sprite spriteDown;
+    public Sprite spriteLeft;
+    public SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -16,16 +20,22 @@ public class Move : MonoBehaviour
  
          if (Input.GetKey ("w")) {
              pos.y += speed * Time.deltaTime;
+             spriteRenderer.sprite = spriteUp;
          }
          if (Input.GetKey ("s")) {
              pos.y -= speed * Time.deltaTime;
+             spriteRenderer.sprite = spriteDown;
          }
          if (Input.GetKey ("d")) {
              pos.x += speed * Time.deltaTime;
+             spriteRenderer.sprite = spriteLeft;
+             spriteRenderer.flipX = true;
          }
          if (Input.GetKey ("a")) {
              pos.x -= speed * Time.deltaTime;
-         }
+             spriteRenderer.sprite = spriteLeft;
+             spriteRenderer.flipX = false;
+        }
              
  
          transform.position = pos;
