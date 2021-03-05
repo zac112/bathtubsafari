60e20c6b29be6dbd3f5db863b1b5ceb33d0f826b
Erno Lokkila <eolokk@utu.fi>
2021-03-05 21:30:26 +0200

    Scenenvaihto lisätty pääkarttaan.

---
+++ Assets/Script/Paakartta/PointOfInterest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PointOfInterest : MonoBehaviour
+{
+    [SerializeField]
+    string sceneName;
+
+    void OnMouseDown() {
+        SceneManager.LoadScene(sceneName);
+    }
+}
