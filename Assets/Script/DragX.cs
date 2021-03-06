4aace7f0deacd0121d4156aa2fc7e09953c2d2a6
Vingit <valtteri.ingman@dnainternet.net>
2021-03-06 19:54:04 +0200

    nyt vain yksi objekti liikku

--- Assets/Script/DragX.cs
+++ Assets/Script/DragX.cs
@@ -1,25 +1,27 @@
-﻿ using System.Collections;
- using System.Collections.Generic;
- using UnityEngine;
- 
- public class DragX : MonoBehaviour
- {
-     Vector3 first, second;
-     public float speed = 15;
- 
-     void Update()
-     {
-         if (Input.GetMouseButtonDown(0))
-         {
-             first = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, 0, 0));
-         }
- 
-         if (Input.GetMouseButton(0))
-         {
-             second = Camera.main.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, 0, 0));
-             Vector3 diff = second - first;
-             transform.position += diff * speed;
-             first = second;
-         }
-     }
- }
+﻿using UnityEngine;
+
+public class DragX : MonoBehaviour
+{
+	private bool mouseDownOnObject;
+	void Update()
+	{
+		if(!mouseDownOnObject) return;
+		
+		Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		pos.y = transform.position.y;
+		pos.z = 0;
+
+		transform.position = pos;
+	}
+
+	private void OnMouseDown()
+	{
+		mouseDownOnObject = true;
+	}
+
+	private void OnMouseUp()
+	{
+		mouseDownOnObject = false;
+	}
+}
--- Assets/Script/DragY.cs
+++ Assets/Script/DragY.cs
@@ -1,25 +1,27 @@
-﻿ using System.Collections;
- using System.Collections.Generic;
- using UnityEngine;
- 
- public class DragY : MonoBehaviour
- {
-     Vector3 first, second;
-     public float speed = 15;
- 
-     void Update()
-     {
-         if (Input.GetMouseButtonDown(0))
-         {
-             first = Camera.main.ScreenToViewportPoint(new Vector3(0, Input.mousePosition.y, 0));
-         }
- 
-         if (Input.GetMouseButton(0))
-         {
-             second = Camera.main.ScreenToViewportPoint(new Vector3(0, Input.mousePosition.y, 0));
-             Vector3 diff = second - first;
-             transform.position += diff * speed;
-             first = second;
-         }
-     }
- }
+﻿using UnityEngine;
+
+public class DragY : MonoBehaviour
+{
+	private bool mouseDownOnObject;
+	void Update()
+	{
+		if(!mouseDownOnObject) return;
+		
+		Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		
+		pos.x = transform.position.x;
+		pos.z = 0;
+
+		transform.position = pos;
+	}
+
+	private void OnMouseDown()
+	{
+		mouseDownOnObject = true;
+	}
+
+	private void OnMouseUp()
+	{
+		mouseDownOnObject = false;
+	}
+}
