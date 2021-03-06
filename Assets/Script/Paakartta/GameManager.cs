ca191ff0fad248236c6ea5c703f505fd4a40dcd5
Erno Lokkila <eolokk@utu.fi>
2021-03-06 11:22:41 +0200

    Vessa valmis

---
+++ Assets/Script/Paakartta/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.hideFlags = HideFlags.DontSave;
+    }
+
+    public void Complete(Scene s) {
+        foreach (GameObject go in points) {
+            PointOfInterest poi = go.GetComponent<PointOfInterest>();
+            if (poi.SceneIndex() == s.buildIndex){
+                poi.SetPointVisible(true);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            SceneManager.LoadScene("Paakartta");
+        }
+    }
+
+}
--- Assets/Script/Paakartta/PointOfInterest.cs
+++ Assets/Script/Paakartta/PointOfInterest.cs
@@ -6,9 +6,19 @@ using UnityEngine.SceneManagement;
 public class PointOfInterest : MonoBehaviour
 {
     [SerializeField]
-    string sceneName;
+    int scene;
+    bool visible;
 
     void OnMouseDown() {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(scene);
+    }
+
+    public int SceneIndex() {
+        return scene;
+    }
+
+    public void SetPointVisible(bool visible) {
+        this.visible = visible;
+        gameObject.SetActive(visible);
     }
 }
