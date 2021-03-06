52859989a8f655b17bc1c31b90367f7a50765681
Juho Duong <thduon@utu.fi>
2021-03-06 11:55:34 +0200

    Lisää testi laatikoiden asettelulle

---
+++ Assets/Script/SolvedCheck.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolvedCheck : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void checkWin() {
+        bool hole1 = GameObject.Find("Hole1").GetComponent<PlugTile>().getIsPlugged();
+        bool hole2 = GameObject.Find("Hole2").GetComponent<PlugTile>().getIsPlugged();
+        bool hole3 = GameObject.Find("Hole3").GetComponent<PlugTile>().getIsPlugged();
+
+        print("Check win");
+        if (hole1 & hole2 & hole3) {
+            print("Epic");
+        }
+    }
+}
