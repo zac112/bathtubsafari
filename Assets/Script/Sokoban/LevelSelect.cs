6e4b2773443c8b393030ec9026c7b4fbb3bf605d
Oskari Ojamaa <71134757+Sparebox@users.noreply.github.com>
2021-03-06 22:44:43 +0200

    LevelSelect nappi

---
+++ Assets/Script/Sokoban/LevelSelect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+
+    Button levelSelectButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelSelectButton = GameObject.Find("LevelSelect").GetComponent<Button>();
+        levelSelectButton.onClick.AddListener(GoToLevelSelection);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GoToLevelSelection() {
+        Timer.secondsSinceStart = 0;
+        SceneManager.LoadScene(5);
+    }
+}
