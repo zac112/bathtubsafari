dc9c40f316f81dba47494aca9b69b6fd7c266acf
Juho Duong <thduon@utu.fi>
2021-03-06 14:22:38 +0200

    Lisää UI

---
+++ Assets/Script/ResetLevel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResetLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    Button resetLevelButton;
+    void Start()
+    {
+        resetLevelButton = GameObject.Find("ResetLevel").GetComponent<Button>();
+        resetLevelButton.onClick.AddListener(ResetCurrentLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ResetCurrentLevel()
+    {  
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- Assets/Script/Timer.cs
+++ Assets/Script/Timer.cs
@@ -10,11 +10,13 @@ public class Timer : MonoBehaviour
     int minutes;
     [SerializeField]
     int seconds;
+    [SerializeField]
+    Text timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        timer = GameObject.Find("TimerText").GetComponent<Text>();
     }
 
     // Update is called once per frame
@@ -28,5 +30,7 @@ public class Timer : MonoBehaviour
         minutes = Mathf.FloorToInt(secondsSinceStart / 60);
         seconds = Mathf.FloorToInt(secondsSinceStart % 60);
         
+        timer.text = "win";
+        print(timer.text);
     }
 }
