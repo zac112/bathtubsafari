5201c0000672bd262e7e8f6e0589a182de0bd6ad
Katja Kaulo <katkau@utu.fi>
2021-03-06 17:20:10 +0200

    public lisätty. Turhaa pohdintaa poistettu.

--- Assets/Script/Paakartta/ValintaHuomio.cs
+++ Assets/Script/Paakartta/ValintaHuomio.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class ValintaHuomio : MonoBehaviour
 {
-    // Täytyiskö tässä luoda siis se "sijainti", sillee new Vector3. Ei, koska tämä on vaan se liike?  emt. tutkitaan
-    // transform.position = new Vector3(x, y, z); mut tonne starttiin siis? tai ihan vaan
-    // Vector3 sijainti;
 
-    float aikaLiikkeenValissa = 1f;
+    public float aikaLiikkeenValissa = 1f;
 
     // Start is called before the first frame update
     void Start()
