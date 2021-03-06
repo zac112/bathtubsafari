70ce5f00de39dce7c1cee1798f862b96b4464633
Oskari Ojamaa <71134757+Sparebox@users.noreply.github.com>
2021-03-06 14:18:34 +0200

    Ajastin skripti

---
+++ Assets/Script/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    
+    [SerializeField]
+    int minutes;
+    [SerializeField]
+    int seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate() {
+        float secondsSinceStart = Mathf.Round(Time.unscaledTime);
+        minutes = Mathf.FloorToInt(secondsSinceStart / 60);
+        seconds = Mathf.FloorToInt(secondsSinceStart % 60);
+        
+    }
+}
