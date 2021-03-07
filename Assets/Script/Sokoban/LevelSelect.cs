68a17e4fb59dca9b01b0fc12098bd4d38133cab8
Oskari Ojamaa <71134757+Sparebox@users.noreply.github.com>
2021-03-07 14:28:49 +0200

    Taso 10 buildsettings

--- Assets/Script/Sokoban/LevelSelect.cs
+++ Assets/Script/Sokoban/LevelSelect.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class LevelSelect : MonoBehaviour
 {
-
+    [SerializeField]
     Button levelSelectButton;
     // Start is called before the first frame update
     void Start()
