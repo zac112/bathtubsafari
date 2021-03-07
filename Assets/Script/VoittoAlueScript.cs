0eb9b568447df0a5185de64718a5fcf96919e4fc
ninosalonen <ninosalonen@gmail.com>
2021-03-07 22:30:34 +0200

    lisätty ernon koodinpätkä

--- Assets/Script/VoittoAlueScript.cs
+++ Assets/Script/VoittoAlueScript.cs
@@ -5,20 +5,16 @@ using UnityEngine.UI;
 
 public class VoittoAlueScript : MonoBehaviour {
 
-	// Reference to WinText game object
 	public Text VoittoTeksti;
     
-
-	// Use this for initialization
 	void Start () {
-		// Turn "You Win!" sign at the start
 		VoittoTeksti.gameObject.SetActive (false);
 	}
 
-	// If target car (red one) exits the playground
 	void OnTriggerEnter2D (Collider2D col)
 	{
-		// then "You Win!" sign is turned on
+		GameObject go = GameObject.Find("GameManager");
+        if(go != null) go.GetComponent<GameManager>().LisaaPiste();
 		VoittoTeksti.gameObject.SetActive (true);
 	}
 }
