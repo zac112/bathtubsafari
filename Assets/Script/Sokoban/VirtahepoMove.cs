93e1db3c9d6739e9cabe897c465ac46e74f83a44
Juho Duong <thduon@utu.fi>
2021-03-07 13:53:53 +0200

    Lisää taso 9 (virtahevot)

---
+++ Assets/Script/Sokoban/VirtahepoMove.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VirtahepoMove : MonoBehaviour
+{
+
+    [SerializeField]
+    Vector3 startVector;
+    [SerializeField]
+    Vector3 endVector;
+    [SerializeField]
+    float speed;
+    bool startToEnd = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (startToEnd)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endVector, Time.deltaTime * speed);
+
+            if (transform.position == endVector)
+            {
+                TurnBack();
+            }
+
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, startVector, Time.deltaTime * 3.0f);
+
+            if (transform.position == startVector)
+            {
+                TurnBack();
+            }
+        }
+
+    }
+
+    void TurnBack()
+    {
+        startToEnd = !startToEnd;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.tag.Equals("Player")) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        TurnBack();
+    }
+}
