b60b83ba764122b83492e8fb831685265760b325
Dogedox <viplin@utu.fi>
2021-03-07 19:40:25 +0200

    tasoentry scriptit

---
+++ Assets/Script/Sokoban/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class test : MonoBehaviour
+{
+    void Start()
+    {
+        muutaVari();
+    }
+    private static Dictionary<int,bool> lapaistytTasot = new Dictionary<int, bool>();
+    public static void isSolved(){
+        lapaistytTasot.Add((SceneManager.GetActiveScene().buildIndex-3),true);
+    }
+    public static void muutaVari(){
+        foreach(var i in lapaistytTasot){
+            GameObject.Find("TasoEntry"+i.Key).GetComponent<SpriteRenderer>().color = new Color(0,1,0,1);
+        }
+    }
+
+}
