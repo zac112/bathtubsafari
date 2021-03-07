366df5a3c122348e33fc6c938aa73c08b294f578
Oskari Ojamaa <71134757+Sparebox@users.noreply.github.com>
2021-03-07 17:58:32 +0200

    Back nappi pääkarttalle lisätty

---
+++ Assets/Script/Sokoban/BackButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    [SerializeField]
+    Button backButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backButton = GameObject.Find("BackButton").GetComponent<Button>();
+        backButton.onClick.AddListener(GoBack);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GoBack() {
+        SceneManager.LoadScene("Paakartta");
+    }
+}
